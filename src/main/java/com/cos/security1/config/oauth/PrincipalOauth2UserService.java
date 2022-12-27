00af10c098144b9a2b7ddbd1a330cf05f67ecb35
Hyunsoo Lee <wrkbrs@gmail.com>
2022-12-27 23:02:53 +0900

    10강 페이스북 로그인 완료

--- src/main/java/com/cos/security1/config/oauth/PrincipalOauth2UserService.java
+++ src/main/java/com/cos/security1/config/oauth/PrincipalOauth2UserService.java
@@ -1,6 +1,9 @@
 package com.cos.security1.config.oauth;
 
 import com.cos.security1.config.auth.PrincipalDetails;
+import com.cos.security1.config.oauth.provider.FacebookUserInfo;
+import com.cos.security1.config.oauth.provider.GoogleUserInfo;
+import com.cos.security1.config.oauth.provider.OAuth2UserInfo;
 import com.cos.security1.model.User;
 import com.cos.security1.repository.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
@@ -39,19 +42,40 @@ public class PrincipalOauth2UserService extends DefaultOAuth2UserService {
         System.out.println("OAuth2User oAuth2User = super.loadUser(userRequest);");
         System.out.println("oAuth2User.getAttributes() = " + oAuth2User.getAttributes());
 
+        OAuth2UserInfo oAuth2UserInfo = null;
+        if(userRequest.getClientRegistration().getRegistrationId().equals("google")) {
+            System.out.println("구글 로그인 요청");
+            oAuth2UserInfo = new GoogleUserInfo(oAuth2User.getAttributes());
+
+        } else if (userRequest.getClientRegistration().getRegistrationId().equals("facebook")) {
+            System.out.println("페이스북 로그인 요청");
+            oAuth2UserInfo = new FacebookUserInfo(oAuth2User.getAttributes());
+
+        } else {
+            System.out.println("구글과 페이스북만 지원합니다.");
+            return null;
+        }
+
         // 회원가입을 강제로 진행할 예정
-        String provider = userRequest.getClientRegistration().getClientId(); // google
-        String providerId = oAuth2User.getAttribute("sub"); // 102921089596693885555
-        String username = provider + "_" + providerId; // google_102921089596693885555
-        String password = bCryptPasswordEncoder.encode("HyunsooLee"); // 여기서 패스워드는 의미는 없지만 배우는 과정이니 생성해준다.
-        String email = oAuth2User.getAttribute("email");
+//        String provider = userRequest.getClientRegistration().getRegistrationId(); // google
+//        String providerId = oAuth2User.getAttribute("sub"); // google: sub, facebook: id
+//        String username = provider + "_" + providerId; // google_102921089596693885555
+//        String password = bCryptPasswordEncoder.encode("HyunsooLee"); // 여기서 패스워드는 의미는 없지만 배우는 과정이니 생성해준다.
+//        String email = oAuth2User.getAttribute("email");
+//        String role = "ROLE_USER";
+
+        String provider = oAuth2UserInfo.getProvider();
+        String providerId = oAuth2UserInfo.getProviderId();
+        String username = provider + "_" + providerId;
+        String password = bCryptPasswordEncoder.encode("HyunsooLee");
+        String email = oAuth2UserInfo.getEmail();
         String role = "ROLE_USER";
 
         // 기존에 유저가 회원가입이 되어있는지 검증
         User userEntity = userRepository.findByUsername(username);
 
         if (userEntity == null) {
-            System.out.println("구글 로그인이 최초입니다.");
+            System.out.println("OAuth2 로그인이 최초입니다.");
             userEntity = User.builder()
                     .username(username)
                     .password(password)
@@ -63,7 +87,7 @@ public class PrincipalOauth2UserService extends DefaultOAuth2UserService {
 
             userRepository.save(userEntity);
         } else {
-            System.out.println("구글 로그인을 이미 한적이 있습니다. 당신은 자동 회원가입이 이미 되어 있습니다.");
+            System.out.println("OAuth2 로그인을 이미 한적이 있습니다. 당신은 자동 회원가입이 이미 되어 있습니다.");
         }
 
         PrincipalDetails principalDetails = new PrincipalDetails(userEntity, oAuth2User.getAttributes());
---
+++ src/main/java/com/cos/security1/config/oauth/provider/FacebookUserInfo.java
@@ -0,0 +1,33 @@
+package com.cos.security1.config.oauth.provider;
+
+import java.util.Map;
+
+public class FacebookUserInfo implements OAuth2UserInfo {
+
+    // oAuth2User.getAttributes() 를 받는다.
+    private Map<String, Object> attributes;
+
+    public FacebookUserInfo(Map<String, Object> attributes) {
+        this.attributes = attributes;
+    }
+
+    @Override
+    public String getProviderId() {
+        return (String) attributes.get("id");
+    }
+
+    @Override
+    public String getProvider() {
+        return "facebook";
+    }
+
+    @Override
+    public String getEmail() {
+        return (String) attributes.get("email");
+    }
+
+    @Override
+    public String getName() {
+        return (String) attributes.get("name");
+    }
+}
